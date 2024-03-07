c5e4adec0b3a7b2f9fabf360c82bcf869e18dfb6
utkarsh032 <utkarshraj525@gmail.com>
2024-03-07 18:31:55 +0530

    frontend Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,13 @@
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline text-center ">
-      Hello Expenser
-    </h1>
+    <div className="h-screen  bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+          Join the waitlist
+        </h1>
+      </div>
+    </div>
   )
 }
 
--- path.md
+++ path.md
@@ -1,2 +1,3 @@
-npm install express express-session graphql @apollo/server @graphql-tools/merge bcryptjs connect-mongodb-session dotenv graphql-passport passport mongoose nodemon concurrently
+backend : npm install express express-session graphql @apollo/server @graphql-tools/merge bcryptjs connect-mongodb-session dotenv graphql-passport passport mongoose nodemon concurrently
 
+frontend: npm install graphql @apollo/client react-router-dom react-icons react-hot-toast chart.js react-chartjs-2 @tailwindcss/aspect-ratio  
