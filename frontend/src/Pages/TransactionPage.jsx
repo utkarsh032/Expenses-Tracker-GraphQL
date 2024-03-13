d8a8004baee94e0707a53c850b4644a7d79d68e1
utkarsh032 <utkarshraj525@gmail.com>
2024-03-13 19:10:40 +0530

    fix refetchQueries

--- frontend/src/Pages/TransactionPage.jsx
+++ frontend/src/Pages/TransactionPage.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import toast from "react-hot-toast";
 import TransactionSkeleton from "../components/Skeltons/TransactionSkelton";
 import { UPDATE_TRANSACTION } from "../graphql/mutations/TransactionMutation";
-import { GET_TRANSACTION } from "../graphql/queries/TransactionQuery";
+import { GET_TRANSACTION, GET_TRANSACTION_STATISTICS } from "../graphql/queries/TransactionQuery";
 
 const TransactionPage = () => {
   const { id } = useParams();
@@ -12,7 +12,9 @@ const TransactionPage = () => {
     variables: { id: id },
   });
 
-  const [updateTransaction, { loading: loadingUpdate }] = useMutation(UPDATE_TRANSACTION);
+  const [updateTransaction, { loading: loadingUpdate }] = useMutation(UPDATE_TRANSACTION, {
+    refetchQueries: [{ query: GET_TRANSACTION_STATISTICS }]
+  });
 
   const [formData, setFormData] = useState({
     description: data?.transaction?.description || "",
